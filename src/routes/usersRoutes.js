346a84f4a819af8eda187d426d0c9fcf024143a5
Андрій <gapeev1979@gmail.com>
2026-09-21 21:25:15 +0300

    add midleware authentscate

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -13,8 +13,12 @@ import {
   updateUserSchema,
   userIdParamSchema,
 } from '../validations/usersValidation.js';
+import { authenticate } from '../middleware/authenticate.js';
 
 const router = Router();
+
+// 2. Додаємо middleware до всіх шляхів, що починаються з /users
+router.use('/users', authenticate);
 
 router.get('/users', celebrate(getUsersSchema), getUsers);
 router.get('/users/:userId', celebrate(userIdParamSchema), getUserById);
